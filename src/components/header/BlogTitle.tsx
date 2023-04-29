02d62be30a95683fc99038d4ab40dd0fbdfee4e7
hae9 <gorud0929@naver.com>
2023-04-29 17:29:40 +0900

    Feat : 헤더제목 폰트 수정

--- src/components/header/BlogTitle.tsx
+++ src/components/header/BlogTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function BlogTitle() {
   return (
-    <h1 className="justify-self-center flex gap-2 text-5xl font-bold font-serif">
+    <h1 className="justify-self-center flex gap-2 text-5xl font-bold font-sans">
       <span className="text-uRed">Hae9</span>
       <span className="text-uPrimary">Blog</span>
     </h1>
